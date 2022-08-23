591c8d422fc29b113b9ddbd2600d296988e20029
lishuailibertine <lishuai19@yeah.net>
2022-08-23 17:44:46 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FlowSwift",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "FlowSwift",
+            targets: ["FlowSwift"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.18.0"),
+        .package(url: "https://github.com/grpc/grpc-swift", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "FlowSwift",
+            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf"), .product(name: "GRPC", package: "grpc-swift")]),
+        .testTarget(
+            name: "FlowSwiftTests",
+            dependencies: ["FlowSwift"]),
+    ]
+)
---
+++ Tests/FlowSwiftTests/FlowSwiftTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import FlowSwift
+
+final class FlowSwiftTests: XCTestCase {
+    func testExample() throws {
+        
+        
+    }
+}
