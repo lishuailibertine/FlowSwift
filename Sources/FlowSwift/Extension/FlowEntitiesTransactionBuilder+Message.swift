51e970640631b108dca587b483d2365400c1edee
lishuailibertine <lishuai19@yeah.net>
2022-09-08 14:31:26 +0800

    add message

--- Sources/FlowSwift/Extension/FlowEntitiesTransactionBuilder+Message.swift
+++ Sources/FlowSwift/Extension/FlowEntitiesTransactionBuilder+Message.swift
@@ -52,13 +52,57 @@ extension FlowEntitiesTransactionBuilder {
         guard let contractData = JsonCadenceObject(type: .dictionaryType, value:.dictionary(contractValue)).jsonData() else {
             throw FlowTransactionError.buildTransferTransactionError
         }
-        let transferTransaction = Flow_Entities_Transaction.with{
+        let createAccountTransaction = Flow_Entities_Transaction.with{
             $0.script = scriptData
             $0.arguments = [publicKeyData, contractData]
             $0.gasLimit = gasLimit
             $0.payer = payer.addressData
             $0.authorizers.append(authAddress.addressData)
         }
-        return transferTransaction
+        return createAccountTransaction
+    }
+    
+    // addAccountKey
+    public mutating func addAccountKey(authAddress: FlowAddress, payer: FlowAddress, gasLimit: UInt64, accountKey: Flow_Entities_AccountKey) throws -> Flow_Entities_Transaction{
+        
+        guard let scriptData = try? FlowTemplate.content(type: .addAccountKeyTemplate).data(using: .utf8) else {
+            throw FlowTransactionError.buildTransferTransactionError
+        }
+        guard let encodeStr = RLP.encode(accountKey.accountKeyData)?.toHexString() else{
+            throw FlowTransactionError.buildTransferTransactionError
+        }
+        guard let publicKeyData = JsonCadenceObject(type: .stringType, value: .string(encodeStr)).jsonData() else {
+            throw FlowTransactionError.buildTransferTransactionError
+        }
+        
+        let addAccountKeyTransaction = Flow_Entities_Transaction.with{
+            $0.script = scriptData
+            $0.arguments = [publicKeyData]
+            $0.gasLimit = gasLimit
+            $0.payer = payer.addressData
+            $0.authorizers.append(authAddress.addressData)
+        }
+        return addAccountKeyTransaction
+    }
+    
+    //removeAccountKey
+    public mutating func removeAccountKey(authAddress: FlowAddress, keyIndex: Int, payer: FlowAddress, gasLimit: UInt64) throws -> Flow_Entities_Transaction{
+        
+        guard let scriptData = try? FlowTemplate.content(type: .removeAccountKeyTemplate).data(using: .utf8) else {
+            throw FlowTransactionError.buildTransferTransactionError
+        }
+        
+        guard let keyIndexData = JsonCadenceObject(type: .intType, value: .int(keyIndex)).jsonData() else {
+            throw FlowTransactionError.buildTransferTransactionError
+        }
+        
+        let removeAccountKeyTransaction = Flow_Entities_Transaction.with{
+            $0.script = scriptData
+            $0.arguments = [keyIndexData]
+            $0.gasLimit = gasLimit
+            $0.payer = payer.addressData
+            $0.authorizers.append(authAddress.addressData)
+        }
+        return removeAccountKeyTransaction
     }
 }
