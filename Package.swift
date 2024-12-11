f21e9b5990b165a5648a78d36d74eb9ddfd6e683
lishuailibertine <libertine9458@gmail.com>
2024-12-11 15:20:37 +0800

    fix version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0"),
         .package(url: "https://github.com/mathwallet/Secp256k1Swift.git", from: "2.0.0"),
-        .package(url: "https://github.com/grpc/grpc-swift", from: "1.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift", exact: "1.24.2"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.4.2"),
         .package(url: "https://github.com/attaswift/BigInt", from: "5.3.0"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", .upToNextMajor(from: "8.1.1")),
